6fa494cf1e36828660d9963d09797ae82705aed9
AutoCommit <admin@relianoid.com>
2026-03-04 15:37:14 +0100

    Commit c98c6c8ad:
        commit 7074611c988e5c0b32d9c73bcb640bbfcb6096ac
        Author: Jose P. Garcia <jose@relianoid.com>
        Date:   Mon Mar 2 11:35:08 2026 +0100
    
        Bump version to 7.9.0
    
        commit a708da5f160acb0c315499afe31a1c73e7871065
        Author: nevola <laura@relianoid.com>
        Date:   Wed Feb 18 09:17:20 2026 +0100
    
        noid-updater cleanup more obsolete zevenet packages
    
        commit eba0c2b166ac9c3ed148c4775b12c8cefe001a4d
        Author: nevola <laura@relianoid.com>
        Date:   Wed Feb 18 09:13:30 2026 +0100
    
        noid-updater cleanup process improvements
    
        commit 39a8c506383c4cae84be4dcf93d6ecc250de7cfc
        Author: nevola <laura@relianoid.com>
        Date:   Wed Feb 18 09:06:04 2026 +0100
    
        noid-updater support to remove obsolete packages
    
        commit 3ea5e8736b63910a9f7aec8aae84b0f37e956159
        Author: nevola <laura@relianoid.com>
        Date:   Thu Feb 12 17:45:38 2026 +0100
    
        introduce deletion of locks every hour using cron based
    
        commit 7341df2c549a7880672250593b0a94eab1759e5a
        Author: nevola <laura@relianoid.com>
        Date:   Sun Feb 1 17:23:40 2026 +0100
    
        remove option D52to60 in noid-backup #472
    
        commit f0f6ccc4dc56440b23d3fbb091a624404e068242
        Author: nevola <laura@relianoid.com>
        Date:   Tue Jan 20 16:30:33 2026 +0100
    
        enable HSTS for cherokee web server
    
    date: 15:37:14_04/03/26

--- usr/local/relianoid/migrations/004-dh2048_cherokee.sh
+++ usr/local/relianoid/migrations/004-dh2048_cherokee.sh
@@ -21,13 +21,10 @@
 #
 ###############################################################################
 
-#Add DH param to 2048 in cherokee conf
-
 cherokee_conf="/usr/local/relianoid/app/cherokee/etc/cherokee/cherokee.conf"
 
+#Add DH param to 2048 in cherokee conf
 if [[ ! `grep "^vserver\!1\!ssl_dh_length.*" $cherokee_conf` ]]; then
 	echo "DH param not found in cherokee conf, adding"
 	sed -i '/server!user = root/a vserver!1!ssl_dh_length = 2048' $cherokee_conf
 fi
-
-
